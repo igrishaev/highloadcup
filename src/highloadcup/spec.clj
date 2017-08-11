57685289be563cffe9b465c563b8f462c359644c
Ivan Grishaev <ivan@grishaev.me>
2017-08-12 00:18:18 +0300

    spec added

---
+++ src/highloadcup/spec.clj
@@ -0,0 +1,24 @@
+(ns highloadcup.spec
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def :user/id int?)
+(s/def :user/email string?)
+(s/def :user/first_name string?)
+(s/def :user/last_name string?)
+(s/def :user/gender #{"m" "f"})
+(s/def :user/birth_date int?)
+
+(s/def :user/create
+  (s/keys :req-un [:user/id
+                   :user/email
+                   :user/first_name
+                   :user/last_name
+                   :user/gender
+                   :user/birth_date]))
+
+(s/def :user/update
+  (s/keys :opt-un [:user/email
+                   :user/first_name
+                   :user/last_name
+                   :user/gender
+                   :user/birth_date]))
