a3d49b8b6f2031fd40bfa381ae1fa42a032331df
Ivan Grishaev <ivan@grishaev.me>
2017-08-11 23:51:04 +0300

    deps added

--- project.clj
+++ project.clj
@@ -3,7 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 ;; [cprop "0.1.10"]
+                 ;; [clj-time "0.14.0"]
+                 [cheshire "5.7.1"]
+                 [compojure "1.6.0"]
+                 [mount "0.1.11"]
+                 [ring/ring-jetty-adapter "1.6.2"]
+                 [ring/ring-json "0.4.0"]]
+
   :main ^:skip-aot highloadcup.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  :target-path "target/highloadcup.jar")
